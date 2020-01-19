da429c4fb11184ae3d659cfc1913bf0fb466dde9
Mikhail Demchenko <ekzamen_na_5@ukr.net>
2020-01-20 01:04:28 +0200

    Initial commit homework URLSession

---
+++ PrivatExchangeRate/PrivatExchangeRate/FirstViewController.swift
@@ -0,0 +1,124 @@
+//
+//  FirstViewController.swift
+//  PrivatExchangeRate
+//
+//  Created by Mr Jake on 18.01.2020.
+//  Copyright © 2020 Mr Jake. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - IJSON Structure
+/*
+ {
+     "ccy": "USD",
+     "base_ccy": "UAH",
+     "buy": "24.05000",
+     "sale": "24.50980"
+ }
+ */
+
+struct Currency: Codable {
+    let currency: String
+    let baseCurrency: String
+    let buy: Double
+    let sale: Double
+
+    enum CodingKeys: String, CodingKey {
+        case currency = "ccy"
+        case baseCurrency = "base_ccy"
+        case buy
+        case sale
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let currency = try container.decode(String.self, forKey: .currency)
+        self.currency = String(currency)
+        let baseCurrency = try container.decode(String.self, forKey: .baseCurrency)
+        self.baseCurrency = String(baseCurrency)
+        let buy = try container.decode(String.self, forKey: .buy)
+        self.buy = Double(buy)!
+        let sale = try container.decode(String.self, forKey: .sale)
+        self.sale = Double(sale)!
+    }
+}
+
+class FirstViewController: UIViewController {
+
+    // MARK: - IBOutlet Label
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var eurLabel: UILabel!
+    @IBOutlet weak var rurLabel: UILabel!
+    @IBOutlet weak var btcLabel: UILabel!
+    @IBOutlet weak var usdCurrencyLabel: UILabel!
+    @IBOutlet weak var eurCurrencyLabel: UILabel!
+    @IBOutlet weak var rurCurrencyLabel: UILabel!
+    @IBOutlet weak var btcCurrencyLabel: UILabel!
+
+    // MARK: - Variable
+    var currencyName: [String] = []
+    var currencyValueBuy: [String] = []
+    var currencyValueSale: [String] = []
+
+    let urlString = "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createDecode(url: urlString)
+    }
+
+    // MARK: - IBAction refreshDataButton
+    @IBAction func refreshDataButton(_ sender: UIBarButtonItem) {
+        createDecode(url: urlString)
+        //fillLabel()
+    }
+
+    // MARK: - Functions
+    private func createDecode(url: String) {
+        guard let url = URL(string: url) else { return }
+
+        URLSession
+            .shared
+            .dataTask(with: url) { (data, response, error) in
+                guard let response = response as? HTTPURLResponse else { return }
+                if let data = data, (200...299).contains(response.statusCode) {
+                    self.handle(responseData: data)
+                }
+                if let _ = data, (300...600).contains(response.statusCode) {
+                    print("IT'S NOT OK!!! ERROR")
+                }
+        }.resume()
+    }
+
+    private func handle(responseData: Data) {
+        let currency = try! JSONDecoder().decode([Currency].self, from: responseData)
+        currencyName = currency.map { (cur) -> String in
+            return cur.currency
+        }
+        currencyValueBuy = currency.map { (cur) -> String in
+            return String(cur.buy)
+        }
+        currencyValueSale = currency.map { (cur) -> String in
+            return String(cur.sale)
+        }
+        DispatchQueue.main.async {
+            self.fillLabel()
+        }
+    }
+
+    private func fillLabel(){
+        usdLabel.text = currencyName[0]
+        eurLabel.text = currencyName[1]
+        rurLabel.text = currencyName[2]
+        btcLabel.text = currencyName[3]
+
+        let btcUahBuy = Double(currencyValueBuy[3])! * Double(currencyValueBuy[0])!
+        let btcUahSell = Double(currencyValueSale[3])! * Double(currencyValueSale[0])!
+
+        usdCurrencyLabel.text = String(currencyValueBuy[0]) + " / " + String(currencyValueSale[0])
+        eurCurrencyLabel.text = String(currencyValueBuy[1]) + " / " + String(currencyValueSale[1])
+        rurCurrencyLabel.text = String(currencyValueBuy[2]) + " / " + String(currencyValueSale[2])
+        btcCurrencyLabel.text = String(format: "%.2f", btcUahBuy) + " / " + String(format: "%.2f", btcUahSell)
+    }
+}
---
+++ PrivatExchangeRate/PrivatExchangeRate/SecondViewController.swift
@@ -0,0 +1,164 @@
+//
+//  SecondViewController.swift
+//  PrivatExchangeRate
+//
+//  Created by Mr Jake on 18.01.2020.
+//  Copyright © 2020 Mr Jake. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - IJSON Structure
+/*
+ {
+     "name": "Черкасское ГРУ",
+     "state": "Черкасская",
+     "id": "2196",
+     "country": "Украина",
+     "city": "Черкассы",
+     "index": "18023",
+     "phone": "8(0472)54-40-80",
+     "email": "cs@pbank.com.ua",
+     "address": "ул Благовестная 169"
+ }
+ */
+
+struct Office: Codable {
+    let name: String
+    let state: String
+    let id: String
+    let country: String
+    let city: String
+    let index: String
+    let phone: String
+    let email: String
+    let address: String
+}
+
+
+class SecondViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    // MARK: - IBOutlet
+    @IBOutlet weak var officesTableView: UITableView!
+
+    // MARK: - Variables
+    var choices = ["","Черкаси","Кіровоград","Сміла","Ужгород","Житомир"]
+    var pickerView = UIPickerView()
+    var typeValue = "Черкассы"
+    var office = [Office]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.requestOffices(city: self.typeValue)
+        DispatchQueue.main.async {
+            self.officesTableView.reloadData()
+        }
+        officesTableView.delegate = self
+        officesTableView.dataSource = self
+    }
+
+    // MARK: - IBAction showPickerViewAction
+    @IBAction func showPickerViewAction(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Вибір міста", message: "\n\n\n\n\n\n", preferredStyle: .alert)
+
+        let pickerFrame = UIPickerView(frame: CGRect(x: 5, y: 20, width: 250, height: 140))
+
+        alert.view.addSubview(pickerFrame)
+        pickerFrame.dataSource = self
+        pickerFrame.delegate = self
+
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (UIAlertAction) in
+            self.requestOffices(city: self.typeValue)
+        }))
+        self.present(alert,animated: true, completion: nil )
+    }
+
+    // MARK: - Functions
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return choices.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return choices[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch row {
+        case 1: typeValue = "Черкассы"
+        case 2: typeValue = "Кировоград"
+        case 3: typeValue = "Смела"
+        case 4: typeValue = "Ужгород"
+        case 5: typeValue = "Житомир"
+        default: typeValue = ""
+        }
+    }
+
+    // https://api.privatbank.ua/p24api/pboffice?json&city=Черкассы
+    private func requestOffices(city: String) {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.privatbank.ua"
+        components.path = "/p24api/pboffice"
+        components.queryItems = [URLQueryItem(name: "json", value: nil),
+                                 URLQueryItem(name: "city", value: city)]
+        guard let url = components.url else { return }
+
+        URLSession
+            .shared
+            .dataTask(with: url) { (data, response, error) in
+                guard let response = response as? HTTPURLResponse else { return }
+                if let data = data, (200...299).contains(response.statusCode) {
+                    self.handle(responseData: data)
+                }
+                if let _ = data, (300...700).contains(response.statusCode) {
+                    print("IT'S NOT OK!!! ERROR")
+                }
+        }.resume()
+    }
+
+    private func handle(responseData: Data) {
+        office = try! JSONDecoder().decode([Office].self, from: responseData)
+        DispatchQueue.main.async {
+            self.officesTableView.reloadData()
+        }
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension SecondViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        office.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        cell.textLabel?.text = office[indexPath.row].name.capitalized
+        cell.detailTextLabel?.text = office[indexPath.row].address.capitalized + "; " + office[indexPath.row].phone.capitalized 
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        let label = UILabel()
+        label.text = office.first?.city
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        return view
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension SecondViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return office.first?.city
+    }
+}
